3a54247e114112d22880be6a0f2a275d06b9cc18
imtiajinfo <imtiaj.info@gmail.com>
2018-03-05 19:07:09 +0600

    first laravel authapp

---
+++ app/Http/Controllers/Auth/ChangePasswordController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class ChangePasswordController extends Controller
+{
+    public function __construct(){
+    	$this->middleware('auth');
+    }
+
+    public function index()
+    {
+    	return view('auth.passwords.change');
+    }
+
+    public function changePassword(Request $request)
+    {
+    	$this->validate($request,[
+    		'oldpassword' => 'required',
+    		'password' => 'required|confirmed'
+    	]);
+
+    	$hashedPassword = Auth::user()->password;
+    	if(Hash::check($request->oldpassword,$hashedPassword)){
+               $user = User::find(Auth::id());
+               $user->password = Hash::make($request->password);
+               $user->save();
+               Auth::logout();
+               return redirect()->route('login')->with('successMsg',"Password Change Successfully");
+    	}else{
+    		return redirect()->back()->with('errorMsg',"Current Password is Invalid");
+    	}
+    }
+}
---
+++ resources/views/auth/passwords/change.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+
+     @if(session('errorMsg'))
+           <div class="alert alert-warning alert-dismissible fade show" role="alert">
+              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+              <strong>Oh Snap!</strong>{{ session('errorMsg') }}
+            </div>
+        @endif
+    <div class="row justify-content-center">
+
+        <div class="col-md-8">
+            <div class="card card-default">
+                <div class="card-header">Register</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('password.update') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="oldpassword" class="col-md-4 col-form-label text-md-right">Old Pasword</label>
+
+                            <div class="col-md-6">
+                                <input id="oldpassword" type="password" class="form-control{{ $errors->has('oldpassword') ? ' is-invalid' : '' }}" name="oldpassword" required autofocus>
+
+                                @if ($errors->has('oldpassword'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('oldpassword') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Confirm Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Update
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
